664b28f4cd996fe2d7aaa6099fe6ec58413740a3
Robert Shields <you@example.com>
2022-05-16 22:13:08 -0700

    added admin and entries

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,13 @@ class Topic(models.Model):
     def __str__(self):
         return self.text
 
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
